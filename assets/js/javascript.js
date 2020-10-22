c06459135bbd9e8abf00d762c7d1d5bafa3823fa
CodingNav <navleenkaurr@gmail.com>
2020-10-22 18:31:58 -0400

    Set up correct and incorrect answers, added questions json

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,24 +1,77 @@
+var questionsUrl = "https://my-json-server.typicode.com/";
 var quiz_one = [
     {
        question : "Where is the digit 8 in 348?",
        answer : "ones place",
-       choices : ["ones place","tens place","hundreds place"]
-    }
+       choices : ["ones place","tens place","hundreds place"],
+       feedback : "Correct answer is ones place."
+    },
+    {
+        question : "Where is the digit 8 in 587?",
+        answer : "tens place",
+        choices : ["ones place","tens place","hundreds place"],
+        feedback : "Correct answer is tens place."
+     }
 ];
+
+var correctMessages = ["Brilliant!", "Awesome!", "Good work!", "Wonderful!"];
+
+var questionIndex = 0;
+var currentQuiz = quiz_one;
 
 function startQuiz(){
     render_view("#question-template",0);
 }
 
+function checkAnswer(event){
+    event.preventDefault();
+    var currentQuestion = currentQuiz[questionIndex];
+    var questionForm = document.getElementById("question");
+    var userChoice = questionForm.querySelector("input:checked");
+    if (userChoice != null && userChoice.value==currentQuestion.answer) {
+        var randomIndex = Math.floor(Math.random()*correctMessages.length);
+        var message = correctMessages[randomIndex];
+        correct_view("#correct-template", message);
+        setTimeout(function(){
+             questionIndex++;
+        render_view("#question-template", questionIndex);   
+        },1000);
+
+    }
+    else {
+        alert("Wrong Answer");
+        render_view("#feedback-template", questionIndex);
+    }
+}
+
 var render_view = (view_id, quiz_index) => {
-    console.log("Rendering View");
     var source = document.querySelector(view_id).innerHTML;
     var template = Handlebars.compile(source);
+    if (quiz_one[quiz_index]) {
+
+    }
     var html = template(quiz_one[quiz_index]);
+  
+    document.querySelector("#view-widget").innerHTML = html;
+  }
+
+
+var correct_view = (view_id, msg) => {
+    var source = document.querySelector(view_id).innerHTML;
+    var template = Handlebars.compile(source);
+  
+    var html = template({
+        message: msg
+    });
   
     document.querySelector("#view-widget").innerHTML = html;
   }
 
 function loadQuiz(){
     render_view("#quiz-starter");
+  }
+
+  function nextQuestion(){
+    questionIndex++;
+    render_view("#question-template", questionIndex);
   }
